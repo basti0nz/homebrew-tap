d3b42f0be776bd528fc2fa1df9ee5322d67e9de6
versus <versus.dev@gmail.com>
2021-05-02 17:10:39 +0300

    Brew formula update for tfselect version 0.1.14

--- Formula/tfselect.rb
+++ Formula/tfselect.rb
@@ -5,16 +5,16 @@
 class Tfselect < Formula
   desc "The tfselect command lets you select terraform versions."
   homepage "https://basti0nz.github.io/tfselect"
-  version "0.1.12"
+  version "0.1.14"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/basti0nz/tfselect/releases/download/0.1.12/tfselect_0.1.12_darwin_amd64.tar.gz"
-    sha256 "ae58f74ad352745fe98c80bbd91796e3995656f794c091a76fa968e52ce5e763"
+    url "https://github.com/basti0nz/tfselect/releases/download/0.1.14/tfselect_0.1.14_darwin_amd64.tar.gz"
+    sha256 "39965d2b62b482294e170bd71f746c355d1a65f550ab5d8c1b9b65198621cbbb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/basti0nz/tfselect/releases/download/0.1.12/tfselect_0.1.12_linux_amd64.tar.gz"
-    sha256 "63efae321b92061e42dd60ab0bae8edb8c01f86107b8ea7312d09c3f3650b2df"
+    url "https://github.com/basti0nz/tfselect/releases/download/0.1.14/tfselect_0.1.14_linux_amd64.tar.gz"
+    sha256 "c2c55efdeebee267bbb4b2a9cc66ab37090882beb9523b9b77e480e6bcc84f6f"
   end
 
   conflicts_with "terraform"
